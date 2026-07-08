f3ba9b18b0490093a4c7ca24d94f3181f699a7ac
Shivali-11 <tambishivali@gmail.com>
2026-07-08 21:59:32 +0530

    Create registration.php

---
+++ 25ESKCA215/DAY-11/registration.php
@@ -0,0 +1,31 @@
+<?php
+include ("header.php");
+include ("checkRegistrationError.php");
+?>
+
+<div class="container mt-5" style="max-width:400px;">
+   <form action="" method="POST">
+        <h3 class="mb-3">Register</h3>
+
+        <input type="text" class="form-control mb-3" name="name" placeholder="Name">
+
+        <input type="email" class="form-control mb-3" name="email" placeholder="email">
+    
+        <input type="text" class="form-control mb-3" name="phone_no" placeholder="phone no.">
+    
+        <input type="password" class="form-control mb-3" name="password" placeholder="password">
+    
+        <input type="password" class="form-control mb-3" name="confirmpassword" placeholder="confirm password">
+    
+        <button type="submit" class="btn btn-primary w-100">Register</button>
+
+    </form>
+</div>
+
+<?php
+    include("footer.php");
+?>
+    
+    
+    
+    
